058bd7d3be05f3ffb4de48cb69536bde066896c9
arhipochki <36302108+arhipochki@users.noreply.github.com>
2022-02-15 08:57:51 +0300

    Update Program.cs

--- FirstProgLab/Program.cs
+++ FirstProgLab/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.Write("Input the 1st num: ");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the 1st num: ");
+int num = Convert.ToInt32(Console.ReadLine()); // First num, the main num. Will be used in future.
 
 Console.Write("Input the 2st num: ");
-int another_num = Convert.ToInt32(Console.ReadLine());
+int another_num = Convert.ToInt32(Console.ReadLine()); // Second num, just for math operations.
 
 Console.WriteLine($"Addition: {num} + {another_num} = {num + another_num}");
 Console.WriteLine($"Subtraction: {num} - {another_num} = {num - another_num}");
@@ -16,8 +16,6 @@ int[] array = new int[num];
 Console.WriteLine("Array: ");
 for (int i = 0; i < num; i++)
 {
-    array[i] = new Random().Next(num * another_num);
+    array[i] = new Random().Next(num * num); 
     Console.Write($"{array[i]} ");
 }
-
-
