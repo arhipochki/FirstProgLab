f7e67314eafeb18cb87dac5268867d5fc86595e3
arhipochki <alexey.arkhipov0@gmail.com>
2022-02-15 08:53:47 +0300

    Добавьте файлы проекта.

---
+++ FirstProgLab/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Input the 1st num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the 2st num: ");
+int another_num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Addition: {num} + {another_num} = {num + another_num}");
+Console.WriteLine($"Subtraction: {num} - {another_num} = {num - another_num}");
+Console.WriteLine($"Multiplication: {num} * {another_num} = {num * another_num}");
+Console.WriteLine($"Division: {num} / {another_num} = {num / another_num}");
+Console.WriteLine($"Mod: {num} % {another_num} = {num % another_num}");
+
+
+int[] array = new int[num];
+
+Console.WriteLine("Array: ");
+for (int i = 0; i < num; i++)
+{
+    array[i] = new Random().Next(num * another_num);
+    Console.Write($"{array[i]} ");
+}
+
+
